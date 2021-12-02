c24ee839e83a7ed72f1759375fde6f0e77120757
olek <olek.banasiak8@wp.pl>
2021-12-02 14:44:28 +0100

    bfgfgg

---
+++ logowanie.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>Flod Serials - rejestracja</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body class="body_rejestracja">
+
+
+<from method="POST" class="rej_box" acction="zaloguj.php">
+    <h1>Zaloguj się się</h1>
+    <input type="text" name="login" placeholder="Podaj login"/><br />
+     <input type="text" name="haslo1"  placeholder="Podaj hasło"/><br />
+    <br />
+    <input type="submit" value="Zaloguj się" />
+
+</form>
+
+
+</body>
+</html>
