cf9e47110d3c24a947f80e14973c5777970d0960
olek <olek.banasiak8@wp.pl>
2021-12-02 13:45:38 +0100

    cyceee

--- newsy.php
+++ newsy.php
@@ -1,15 +1,4 @@
-<section class="recentNews">
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="style.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel="icon" type="image/x-icon" href="grafika/miniaturka.svg">
-    <meta name="title" content="Papajak">
-    <meta charset="utf-8">
-</head>
-<body>
+
     <section class="recentNews">
     <div class="container">
         <h2 class="news-title">Newsy ze świata seriali</h2>
@@ -27,18 +16,19 @@
         
         echo '
         
-        <div class="ct-blog col-sm-6 col-md-2">
-            <div class="inner">
-            <div class="fauxcrop">
-                <a href="#"><img src="grafika/'. $serial["zdjecie"] .'" class="img-fluid"></a>
-            </div>
-            <div class="ct-blog-content">
+        <div class="serialbox col-sm-12 d-flex">
+            <img src="grafika/'. $serial["zdjecie"] .'" class="img-fluid">
+
+            <div class="opis serialboxoffice">
                 <h3 class="ct-blog-header">
                 '. $serial["nazwa"] .'
                 </h3>
             </div>
-            <br>
+
+            <div class="ocene ">
+                fsfdfds
             </div>
+
         </div>
         
         ';
