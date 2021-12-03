f08351e0b6a09879b06aa4c8415dfbdfcd1a3392
maro <marek.kubiak2002@gmail.com>
2021-12-03 11:43:32 +0100

    laj laj

--- glowna.php
+++ glowna.php
@@ -1,1 +1,57 @@
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Jestem tego pewny, w głębi duszy o tym wiem
+Że gdzieś na szczycie góry, wszyscy razem spotkamy się
+Mimo świata który, kocha i rani nas dzień w dzień
+Gdzieś na szczycie góry, wszyscy razem spotkamy się
+Się porobiło czeka to też nas
+Ziemia, coraz więcej pragnie ciała ludzkich mas
+Nikt nam nie powiedział, jak długo będziemy żyć
+Do jakich rozmiarów ciągnęła się będzie życia nić
+Nikt nam nie powiedział, kiedy mamy się pożegnać
+I ile mamy czekać aby znowu się pojednać
+Ramię w ramię nawzajem siebie wspierać
+Rodzimy się by żyć, żyjemy by umierać
+Aniele śmierci proszę powiedz mi
+Czemu w stosunku do nas jesteś obojętny
+Najlepsze są dla ciebie młode ofiary
+Nigdy nic Ci nie zrobiły a traktujesz je jak psy
+Zobacz!
+Ile miłości w każdym człowieku
+Nie jeden by chciał przeżyć choć pół wieku
+Nie jednemu od wielu lat najbliższych ziomów brak
+Ciężko pogodzić się z tym, ale mimo tego ja i tak
+Jestem tego pewny, w głębi duszy o tym wiem
+Że gdzieś na szczycie góry, wszyscy razem spotkamy się
+Mimo świata który, kocha i rani nas dzień w dzień
+Gdzieś na szczycie góry, wszyscy razem spotkamy się
+Spotkamy się, spotkamy się, spotkamy się, spotkamy się
+Gdzieś daleko i bardzo wysoko
+Gdzie zwykły śmiertelnik nie stąpa tam nogą
+Gdzie spokój, harmonia i natury zew
+Gdzie słychać szum drzew i ptaków śpiew
+Wschód słońca pada na twarz
+Wypełnia twą duszę, którą ciągle masz
+Wydaje się tobie że te uczucie już znasz, ale
+Ono wcale nie jest ci znane
+Chociaż było pisane i pisane też jest nam
+Tak że wszyscy się spotkamy, więc nie będziesz już sam
+Spotkasz ludzi których tak bardzo kochałeś
+Choć lata nie widziałeś nadal kochać nie przestałeś (Yo!)
+Teraz leżysz na plecach, spoglądasz na błękitne niebo
+Serce dotyka serca, nie ma szczęścia, nie ma pecha
+Nikt się nie złości, nikt nie docieka
+Człowiek obok człowieka z nurtem płyną jak rzeka
+Połączeni w jedną całość na samym szczycie góry, Mekka
+Jestem tego pewny, w głębi duszy o tym wiem
+Że gdzieś na szczycie góry, wszyscy razem spotkamy się
+Mimo świata który, kocha i rani nas dzień w dzień
+Gdzieś na szczycie góry, wszyscy razem spotkamy się
+Jestem tego pewny, w głębi duszy o tym wiem
+Że gdzieś na szczycie góry, wszyscy razem spotkamy się
+Mimo świata który, kocha i rani nas dzień w dzień
+Gdzieś na szczycie góry, wszyscy razem spotkamy się
+Jestem tego pewny, w głębi duszy o tym wiem
+Że gdzieś na szczycie góry, wszyscy razem spotkamy się
+Mimo świata który, kocha i rani nas dzień w dzień
+Gdzieś na szczycie góry, wszyscy razem spotkamy się
+Zróbcie ogromny hałas dla wszystkich, którzy odeszli
+Dziękujemy wam słuchajcie z całego serca
