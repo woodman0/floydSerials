758b3fbf0fa448bd7a3671f940a30afbb1e01465
maro <marek.kubiak2002@gmail.com>
2021-12-08 09:41:28 +0100

    olek nic nie umie, ja to zrobilem

--- glowna.php
+++ glowna.php
@@ -1,59 +1,39 @@
-<p>
-    Jestem tego pewny, w głębi duszy o tym wiem
-    Że gdzieś na szczycie góry, wszyscy razem spotkamy się
-    Mimo świata który, kocha i rani nas dzień w dzień
-    Gdzieś na szczycie góry, wszyscy razem spotkamy się
-    Się porobiło czeka to też nas
-    Ziemia, coraz więcej pragnie ciała ludzkich mas
-    Nikt nam nie powiedział, jak długo będziemy żyć
-    Do jakich rozmiarów ciągnęła się będzie życia nić
-    Nikt nam nie powiedział, kiedy mamy się pożegnać
-    I ile mamy czekać aby znowu się pojednać
-    Ramię w ramię nawzajem siebie wspierać
-    Rodzimy się by żyć, żyjemy by umierać
-    Aniele śmierci proszę powiedz mi
-    Czemu w stosunku do nas jesteś obojętny
-    Najlepsze są dla ciebie młode ofiary
-    Nigdy nic Ci nie zrobiły a traktujesz je jak psy
-    Zobacz!
-    Ile miłości w każdym człowieku
-    Nie jeden by chciał przeżyć choć pół wieku
-    Nie jednemu od wielu lat najbliższych ziomów brak
-    Ciężko pogodzić się z tym, ale mimo tego ja i tak
-    Jestem tego pewny, w głębi duszy o tym wiem
-    Że gdzieś na szczycie góry, wszyscy razem spotkamy się
-    Mimo świata który, kocha i rani nas dzień w dzień
-    Gdzieś na szczycie góry, wszyscy razem spotkamy się
-    Spotkamy się, spotkamy się, spotkamy się, spotkamy się
-    Gdzieś daleko i bardzo wysoko
-    Gdzie zwykły śmiertelnik nie stąpa tam nogą
-    Gdzie spokój, harmonia i natury zew
-    Gdzie słychać szum drzew i ptaków śpiew
-    Wschód słońca pada na twarz
-    Wypełnia twą duszę, którą ciągle masz
-    Wydaje się tobie że te uczucie już znasz, ale
-    Ono wcale nie jest ci znane
-    Chociaż było pisane i pisane też jest nam
-    Tak że wszyscy się spotkamy, więc nie będziesz już sam
-    Spotkasz ludzi których tak bardzo kochałeś
-    Choć lata nie widziałeś nadal kochać nie przestałeś (Yo!)
-    Teraz leżysz na plecach, spoglądasz na błękitne niebo
-    Serce dotyka serca, nie ma szczęścia, nie ma pecha
-    Nikt się nie złości, nikt nie docieka
-    Człowiek obok człowieka z nurtem płyną jak rzeka
-    Połączeni w jedną całość na samym szczycie góry, Mekka
-    Jestem tego pewny, w głębi duszy o tym wiem
-    Że gdzieś na szczycie góry, wszyscy razem spotkamy się
-    Mimo świata który, kocha i rani nas dzień w dzień
-    Gdzieś na szczycie góry, wszyscy razem spotkamy się
-    Jestem tego pewny, w głębi duszy o tym wiem
-    Że gdzieś na szczycie góry, wszyscy razem spotkamy się
-    Mimo świata który, kocha i rani nas dzień w dzień
-    Gdzieś na szczycie góry, wszyscy razem spotkamy się
-    Jestem tego pewny, w głębi duszy o tym wiem
-    Że gdzieś na szczycie góry, wszyscy razem spotkamy się
-    Mimo świata który, kocha i rani nas dzień w dzień
-    Gdzieś na szczycie góry, wszyscy razem spotkamy się
-    Zróbcie ogromny hałas dla wszystkich, którzy odeszli
-    Dziękujemy wam słuchajcie z całego serca
-</p>
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" style="margin: auto; width: 29%;">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="grafika/swinkapeppa.jpg" class="d-block w-100">
+    </div>
+        <?php
+
+        $conn = mysqli_connect("localhost", "root", "", "bazaseriale");
+
+        $sql = "SELECT * FROM seriale";
+
+        $result = mysqli_query($conn, $sql);
+
+        while($serial = mysqli_fetch_assoc($result)) 
+        {
+            
+            echo '
+            
+            <div class="carousel-item" data-bs-interval="1000">
+                <img src="grafika/'.$serial["zdjecie"].'" class="d-block w-100">
+            </div>
+
+            ';
+
+        }
+
+        mysqli_close($conn);
+
+        ?>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">poprzedni</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">nastepny</span>
+  </button>
+</div>
