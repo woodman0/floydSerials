bc3e37df017799eddc32e01e9a25fd9962e224e1
maro <marek.kubiak2002@gmail.com>
2021-12-08 14:18:52 +0100

    es

--- ranking.php
+++ ranking.php
@@ -2,42 +2,51 @@
     include 'top.php';
 ?>
 
-<h2>
-    TOP 3 w rankingu seriali 2021 roku!
-</h2>
 
-<?php
+<section class="recentNews">
+    <div class="wiersz" data-animation="hierarchical-display">
+        <div class="container">
+            <h2>
+                TOP 5 w rankingu seriali 2021 roku!
+            </h2>
 
-$conn = mysqli_connect("localhost", "root", "", "bazaseriale");
+        <?php
+        $i = 1;
+        $conn = mysqli_connect("localhost", "root", "", "bazaseriale");
 
-    $sql = "SELECT * FROM seriale ORDER BY srednia_ocen DESC LIMIT 3";
+            $sql = "SELECT * FROM seriale ORDER BY srednia_ocen DESC LIMIT 5";
 
-    $result = mysqli_query($conn, $sql);
+            $result = mysqli_query($conn, $sql);
 
-    while($serial = mysqli_fetch_assoc($result)) 
-    {
-        
-        echo '
-        
-        <div class="serialbox col-sm-12 d-flex">
-            <img src="grafika/'.$serial["zdjecie"].'" class="img-fluid">
+            while($serial = mysqli_fetch_assoc($result)) 
+            {
+                
+                echo '
+                
+                <div class="serialbox col-sm-12 d-flex">
+                    <h1>'.$i++.'.</h1>
+                    <img src="grafika/'.$serial["zdjecie"].'" class="img-fluid">
 
-            <div class="opisSerialboxoffice">
-                <h3 class="ct-blog-header">
-                '.$serial["nazwa"].'
-                </h3>
-            </div>
+                    <div class="opisSerialboxoffice">
+                        <h3 class="ct-blog-header">
+                        '.$serial["nazwa"].'
+                        </h3>
+                    </div>
+
+                </div>
+                
+                ';
+
+            }
+
+            mysqli_close($conn);
+
+
+        ?>
 
         </div>
-        
-        ';
-
-    }
-
-    mysqli_close($conn);
-
-
-?>
+    </div>
+</section
 
 <?php
     include 'bot.php';
